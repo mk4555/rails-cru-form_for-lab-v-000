617ed2c1e77ca6103600ebdf860ade5839856c11
Min Kim <mk4555@nyu.edu>
2018-02-06 05:09:19 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -14,7 +14,7 @@ class ArtistsController < ApplicationController
   def create
     @artist = Artist.create(params)
 
-    redirect_to(artist_path(@artist))
+    redirect_to artist_path(@artist)
   end
 
   def edit
